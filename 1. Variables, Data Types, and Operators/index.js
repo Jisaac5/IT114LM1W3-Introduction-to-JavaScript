a841c562913789173fddc05d866c7fe5b1e4081c
Jisaac5 <jisaacserote@gmail.com>
2024-01-13 15:13:10 +0800

    Started on Exercise 1

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -5,12 +5,29 @@
 
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
 // Your code here
+var testVar = 1;
+
+console.log("testVar = ", testVar, "\n");
+
+//var testVar = 1;
+
+//Moving the var to after the log line made the log print it out as "undefined".
 
 // TODO 1.2 Use the let keyword to define a variable.
 // Your code here
+let testVar2 = 2;
 
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
+const constVar = 3;
+//constVar = 3.5;
+
+//Trying to reassign the const's value caused an error.
+
+console.log("Testing values for all 3 variables...");
+console.log("testVar = ", testVar);
+console.log("testVar2 = ", testVar2);
+console.log("constVar = ", constVar);
 
 // Checkpoint 1.1 What is the difference between var, let, and const?
 // Answer: 
